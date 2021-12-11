f8df1f21192d62550890f7ccbcfa94770cc16f4a
jetlime <paul.houssel@yahoo.de>
2021-12-11 15:31:02 +0100

    add log file to obtain all the steps the alg. took

--- Horne-Alg/app/Sequents.hs
+++ Horne-Alg/app/Sequents.hs
@@ -2,9 +2,6 @@ module Sequents where
 
 import Parser
 import Utils
--- to use mapWithIndex
-import Data.Sequence (Seq(..), (<|), (|>), (><))
-import qualified Data.Sequence as S
 import Data.Typeable
 import qualified Data.Map as M
 import Data.Map (Map)
@@ -13,9 +10,7 @@ import Data.List (elemIndex)
 -- https://hackage.haskell.org/package/multiset-0.3.4.3/docs/Data-MultiSet.html#g:1
 import Data.MultiSet (MultiSet)
 import qualified Data.MultiSet as MultiSet
--- Debug
-import Debug.Trace
-
+import qualified System.IO.Strict as SIO
 -- okRule using recursion and Data.Multiset
 -- O(n)
 -- if the rule holds, return True
@@ -55,12 +50,13 @@ timesRule sequent = do
 
 findNext :: (MultiSet LocalType) -> LocalType -> Either LocalType LocalType
 findNext sequent (Act Send s ss) = do 
-    -- position of the dual action 
+    -- list of all dual types in the sequent
     let result = MultiSet.filter (isActReceive s) sequent
+    -- if the list is empty then the prefix rule is not applied on this LocalType
     if (MultiSet.null result) then Left (Act Send s ss) else Right (Act Send s ss)
 findNext sequent (Act Receive s ss) = do 
-    -- Position of the dual action 
-    let result = MultiSet.filter isActSend sequent 
+    -- list of all dual types in the sequent
+    let result = MultiSet.filter (isActSend s) sequent 
     if (MultiSet.null result) then Left (Act Receive s ss) else Right (Act Receive s ss)
 
 
@@ -71,10 +67,11 @@ checkPrefix  sequent (Act dir s lt) = do
         -- if another dual action exists
         Right ans -> do 
             Right (Act dir s lt)
-        -- if no dual ction exists
+        -- if no dual action exists
         Left ans -> do 
             Left (Act dir s lt)
 checkPrefix xs lt = Left lt
+
 
 
 prefixRule :: (MultiSet LocalType) ->  Bool
@@ -86,6 +83,48 @@ prefixRule sequent = do
     --if (any isAct xs) then putStrLn(show True) else putStrLn(show False)
     okRule result
 
+
+timesRuleTest:: (MultiSet LocalType) -> (MultiSet LocalType)
+timesRuleTest sequent = do 
+    let result = MultiSet.concatMap (\x -> if isPrl x then [checkTimes x, checkTimes2 x] else [x] ) sequent
+    result
+
+findNext2 :: (MultiSet LocalType) -> LocalType -> IO()
+findNext2 sequent (Act Send s ss) = do 
+    -- list of all dual types in the sequent
+    let result = MultiSet.filter (isActReceive s) sequent
+    print (MultiSet.toList result )
+findNext2 sequent (Act Receive s ss) = do 
+    -- list of all dual types in the sequent
+    let result = MultiSet.filter (isActSend s) sequent 
+    print (MultiSet.toList result)
+findNext2 _ lt = putStrLn "Nothing"    
+
+
+
+prefixBranches :: (MultiSet LocalType) -> IO()
+prefixBranches sequent = do
+    -- given the sequent, [(?a;end, 1),(!a;!b;end, 1),(!a;end, 1)]
+    -- MultiSet.mapMaybe iterates over sequent,
+    --let nlist = MultiSet.mapMaybe (findNext2 sequent) (sequent)
+    --print (MultiSet.toList nlist)
+    putStrLn "paul"
+    -- on ?a;end, it will call findNext, which will generate a list result
+    -- of all the dual actions in the sequent, [!a;!b;end,!a;end], this MultiSet is returned
+    -- 
+    -- given the sequent, generate a list of all the LocalTypes that have a dual in the sequent
+    -- e.G [?a;end, !a;?b;end, !a;end, end]
+    -- becomes [?a;end]
+    -- for every element in this list apply the prefix rule on the sequent
+    -- if find next obtains more then 1 result for find next generate a branching, in one 
+    -- branch the first choice is taken, in the other branch the other choice is taken
+    -- find next will find !a;?b;end and !a;end -> [!a;?b;end,!a;end]
+    -- it will apply the prefix rule on the first element, then continue from scratch, if at the
+    -- end, the end rule does not apply, we backtrack, and start again from the second element
+    -- in the list given, !a;end, if the OK rule holds in this case then the relation holds, if not
+    -- the relation does not hold because not other element his present in this branch list.
+    -- we obtain a list of sequents :
+    
 algorithmRun :: (MultiSet LocalType) -> Bool
 algorithmRun sequent = do 
     okRule sequent
@@ -116,11 +155,17 @@ dualize subtype supertype False = do
 
 sequentsAlg :: LocalType -> LocalType -> Bool ->  IO()
 sequentsAlg subtype supertype mode = do 
+    writeToFile "tmp/log.txt" (printLocalType subtype ++ "<=" ++ printLocalType supertype)
     -- chose which type to dualize in function of mode
     let ans = dualize subtype supertype mode
+    writeToFile "tmp/log.txt" (show(MultiSet.toList ans))
     -- we dualized one of the types.
     -- and put the two types in a Multiset called sequent
     -- start of alg. 
+    -- DEBUG
+    let timedRule = timesRuleTest ans
+    findNext2 timedRule (Act Send "a" End)
+    prefixBranches ans
     let algResult = algorithmRun ans
     -- End Of Algorithm
     printResult subtype supertype algResult 
