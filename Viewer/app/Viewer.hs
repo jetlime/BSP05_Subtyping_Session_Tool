d061c7e5e2472680bb39889387456e08eec15cef
jetlime <paul.houssel@yahoo.de>
2022-01-02 21:45:29 +0100

    debuged the viewer

--- Viewer/app/Viewer.hs
+++ Viewer/app/Viewer.hs
@@ -15,6 +15,7 @@ import Control.Monad
 import Data.Text (strip, pack, unpack)
 import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime)
 import Text.Printf (printf)
+import System.Process
 
 -- DEBUG
 import System.IO.Unsafe
@@ -51,9 +52,12 @@ main = do
              if isPrl ans then do 
               let m1 = type2Machine False "-" ans
               let m2 = type2Machine2 False "-" ans 
-              machine2file m1 "type" 
+              machine2file m1 "type1" 
               machine2file2 m2 m1 "type2"
-              putStrLn "Done" 
+              -- merge the two created .dot files
+              -- type2_csfm.dot & type1_csfm.dot to type_csfm.dot
+              runCommand "gvpack -u -o tmp/type_cfsm.dot tmp/type1_cfsm.dot tmp/type2_cfsm.dot"
+              putStrLn "Done"
              else do
               let m1 = type2Machine False "-" ans
               machine2file m1 "type" 
